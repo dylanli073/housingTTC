e441eb0421850a8dcd94253765669c61a2e28ea9
Dylan Li <dylanli@dhcp-10-250-23-216.harvard.edu>
2019-12-05 01:45:09 -0500

    finished code, not tested

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ num_mapping = {1:"Adams", 2:"Cabot", 3:"Currier", 4:"Dunster", 5:"Eliot", 6:"Kir
     9:"Mather", 10:"Pfoho", 11:"Quincy", 12:"Winthrop"}
 group_prefs = {"Adams":{}, "Cabot": {}, "Currier": {}, "Dunster": {}, "Eliot": {}, "Leverett": {}, "Lowell": {}, 
     "Mather": {}, "Pfoho": {}, "Quincy": {}, "Winthrop": {}} # organized by house, then by group size
+totalTrades = 0
 
 def main():
     total_groups = int(raw_input())
@@ -30,65 +31,125 @@ def main():
         # consider if blocking group name is used before
 
         try:
-            group_prefs[startingHouse][groupSize].append(blocking_name.append(map(int, input[3:])))
+            group_prefs[startingHouse][groupSize].append((blocking_name, (map(int, input[3:]))))
         except:
-            group_prefs[startingHouse][groupSize] = [blocking_name.append(map(int, input[3:]))]
+            group_prefs[startingHouse][groupSize] = [(blocking_name, (map(int, input[3:])))]
 
     # running 8 rounds for all of the possible blocking group sizes
     rounds = 8
-    graph = preprocessing(8)
-    # for groupSize in range(rounds): 
-    #     # preprocessing the lists in dictionary to order by the largest group size first
-    #     graph = preprocessing(groupSize)
-    #     if len(graph) == 0:
-    #         return False
-    #     print("Running group size of size " + str(groupSize))
-    #     runTTC(graph, groupSize)
+    # graph = preprocessing(rounds)
+
+    for groupSize in range(rounds): 
+        # preprocessing the lists in dictionary to order by the largest group size first
+        graph = preprocessing(groupSize)
+        if len(graph) == 0:
+            return False
+        print("Running group size of size " + str(groupSize))
+        runTTC(graph, groupSize)
+
+    return totalTrades
+# def preprocessing(total): 
+#     """preprocessing pairs up items into groups, both by size and the most preferred item available, 
+#     and this output is used as the method by which each round matches and finds cycles
+#     """
+#     pairs = {"Adams":{}, "Cabot": {}, "Currier": {}, "Dunster": {}, "Eliot": {}, "Leverett": {}, "Lowell": {}, 
+#     "Mather": {}, "Pfoho": {}, "Quincy": {}, "Winthrop": {}}
+
+#     # pairing up by group size, according to target
+#     for key, house in group_prefs.items():
+#         frontPtr, endPtr = 0, total
+#         # within each house, comparing groups to see if they would form a pair pointing to same house
+#         while frontPtr <= endPtr:
+#             try: 
+#                 for group in house[frontPtr]:
+#                     for comp_group in house[endPtr]:
+#                         if group[1][0] == comp_group[1][0]:
+#                             names = [group[0], comp_group[0]]
+#                             try:
+#                                 pairs[house][total].append([names, group[1][0]])
+#                             except:
+#                                 pairs[house][total] = [[names, group[1][0]]]
+#             except ValueError:
+#                 pass 
+#             finally:
+#                 frontPtr += 1
+#                 endPtr -= 1
+#     return pairs
 
 
-def preprocessing(total): 
-    """preprocessing pairs up items into groups, both by size and the most preferred item available, 
-    and this output is used as the method by which each round matches and finds cycles
-    """
-    pairs = {"Adams":{}, "Cabot": {}, "Currier": {}, "Dunster": {}, "Eliot": {}, "Leverett": {}, "Lowell": {}, 
-    "Mather": {}, "Pfoho": {}, "Quincy": {}, "Winthrop": {}}
-
-    # pairing up by group size, according to target
-    for key, house in group_prefs.items():
-        frontPtr, endPtr = 0, total
-        # within each house, comparing groups to see if they would form a pair pointing to same house
-        while frontPtr <= endPtr:
-            try: 
-                for group in house[frontPtr]:
-                    for comp_group in house[endPtr]:
-                        if group[1][0] == comp_group[1][0]:
-                            names = [group[0], comp_group[0]]
-                            try:
-                                pairs[house][total].append([names, group[1][0]])
-                            except:
-                                pairs[house][total] = [[names, group[1][0]]]
-            except ValueError:
-                pass 
-            finally:
-                frontPtr += 1
-                endPtr -= 1
-    return pairs
-
-
-def runTTC(round):
+def runTTC(groupSize):
     """Function runs TTC for the blocking groups, accepts preferences and round # as inputs, 
     outputs paired trades, updates global ledger
     """
+    iteration = {"Adams": 0, "Cabot": 0, "Currier": 0, "Dunster": 0, "Eliot": 0, "Kirkland": 0, "Leverett": 0, "Lowell": 0, 
+            "Mather": 0, "Pfoho": 0, "Quincy": 0, "Winthrop": 0}
+    allHouses = set(["Adams", "Cabot", "Currier", "Dunster", "Eliot", "Kirkland", "Leverett", "Lowell", 
+            "Mather", "Pfoho", "Quincy", "Winthrop"])
+
+    # shuffling each list 
+    for house in remainingHouses: 
+        random.shuffle(group_prefs[house][groupSize])
+
+    empty_houses = set()
+
+    halted = False # checks if there is a stall in the TTC, where one group must move down
+    while len(empty_houses) < 11: # some case for when TTC would end for size 8 blocking group size
+        remainingHouses = allHouses.difference(empty_houses)
+        if halted:
+            chosen_house = random.sample(remainingHouses, 1)[0] # move one down ledger for this house
+            iteration[chosen_house] += 1
+            if iteration[chosen_house] >= len(group_prefs[house][groupSize])
+                empty_houses.add(chosen_house)
+                continue
+
+        # adding the names and preference ordering for each node within the graph
+        graph_nodes = []
+        for house in remainingHouses: 
+            associated_houses[house] = group_prefs[house][groupSize][iteration[chosen_house]][0] # name asssociated with each house
+            graph_nodes.append(associated_houses[house]) # returns the tuple associated
+
+        visited = set()
+        graph = preprocessing(graph_nodes)
+        def runDFS(house):
+            """Graph search algorithm for finding cycles, accepts input graph in adjacency list format, outputs cycles
+            and clears the current market, returns number of cycles cleared
+            """
+            visited.add(house)
+            stack = [house]
+            remainingHouses.remove(house)
+            while graph[house] not in visited:
+                house = graph[house]
+                remainingHouses.remove(house)
+                stack.append(graph[house])
+            try: 
+                cycle_start = stack.index(graph[house])
+                for house in range(cycle_start, len(stack)):
+                    iteration[house] += 1
+                    # making the trade here
+
+                    totalTrades += groupSize
+
+                    if iteration[house] >= len(group_prefs[house][groupSize]) # checking if iterations is greater than the 
+                        empty_houses.add(house) # already in visited set, will not be visited later
+                return True
+            except ValueError:
+                return False
+                
+        # based on guarantee that each node has one outgoing edge
+        halted = True
+        while len(remainingHouses) > 0:
+            if runDFS(random.sample(remainingHouses, 1)[0]): 
+                halted = False
 
 
-def removeGroups()
-    """Cleans up and removes groups that have already been used"""
-
-
-def runDFS():
-    """Graph search algorithm for finding cycles, accepts input graph in adjacency list format, outputs cycles
-    and clears the current market, returns number of cycles cleared
+def preprocessing(graph_nodes):
+    """preprocessing creates adjacency list representation for the given length
+        and this output is used as the method by which each round matches and finds cycles
     """
+    adjacency_list = {}
+    for node in graph_nodes: 
+        adjacency_list[node[1][1]] = num_mapping[node[1][2]] # node[1][2] is the top choice house, num_mapping translates to house name
+    return adjacency_list
 
 
 if __name__ == "__main__":
